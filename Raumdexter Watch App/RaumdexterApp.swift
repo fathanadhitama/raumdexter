3fc722b8f5ed5c727a39ead00409ccc710616be1
fathanadhitama <fathan.naufal@ui.ac.id>
2026-09-01 11:27:16 +0700

    Initial Commit

---
+++ Raumdexter Watch App/RaumdexterApp.swift
@@ -0,0 +1,17 @@
+//
+//  RaumdexterApp.swift
+//  Raumdexter Watch App
+//
+//  Created by Fathan Naufal Adhitama on 01/09/26.
+//
+
+import SwiftUI
+
+@main
+struct Raumdexter_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Raumdexter/RaumdexterApp.swift
@@ -0,0 +1,17 @@
+//
+//  RaumdexterApp.swift
+//  Raumdexter
+//
+//  Created by Fathan Naufal Adhitama on 01/09/26.
+//
+
+import SwiftUI
+
+@main
+struct RaumdexterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
